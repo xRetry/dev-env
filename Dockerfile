3c5712b049fa449239e28ad015c71822c1edb7fc
xRetry <marcoschwarz.at@gmail.com>
2023-09-14 22:19:35 +0200

    Allow setup for different users

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu
 
 RUN apt-get update && apt-get install -y curl
-RUN curl https://raw.githubusercontent.com/xRetry/dev-env/main/setup_env.sh | sh -s -- c rust
+RUN curl https://raw.githubusercontent.com/xRetry/dev-env/main/setup_ubuntu.sh | sh -s -- -t marco -l python -l rust -l c
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,8 +2,8 @@
 
 setup_rust() {
 	curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
-	echo "export PATH=~/.cargo/bin:$PATH" >> ~/.bashrc
-	export PATH=~/.cargo/bin:$PATH
+	echo "export PATH=$user/.cargo/bin:$PATH" >> $user/.bashrc
+	export PATH=$user/.cargo/bin:$PATH
 	nvim --headless +'TSInstall rust' +'MasonInstall rust-analyzer' +'sleep 20' +qall
 }
 
@@ -17,38 +17,8 @@ setup_python() {
     nvim --headless +'TSInstall python' +'sleep 20' +qall
 }
 
-apt-get update && apt-get install -y \
-    curl \
-    git \
-    g++ \
-    unzip
- 
-# Bash
-echo "set -o vi" >> ~/.bashrc
-echo "export TERM=screen-256color-bce" >> ~/.bashrc
-
-# Tmux
-apt-get install -y tmux
-# Tmux - Config
-mkdir -p ~/.config/tmux
-git clone https://github.com/xRetry/tmux.git ~/.config/tmux
-
-# Neovim
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
-mkdir -p ~/.local/share/nvim-linux64
-mv nvim-linux64 ~/.local/share/
-ln -s ~/.local/share/nvim-linux64/bin/nvim /usr/bin/nvim
-rm nvim-linux64.tar.gz
-# Neovim - Config
-mkdir -p ~/.config/nvim
-git clone https://github.com/xRetry/nvim.git ~/.config/nvim
-nvim --headless +'Lazy sync' +'sleep 20' +qall
-
-
-while test $# -gt 0
-do
-    case "$1" in
+parse_lang() {
+    case $1 in
         rust) setup_rust
             ;;
         c) setup_c
@@ -56,7 +26,53 @@ do
         python) setup_python
             ;;
     esac
-    shift
+}
+
+setup_tools() {
+    home=$(awk -F: -v v="$1" '{if ($1==v) print $6}' /etc/passwd)
+
+    # Bash
+    echo "set -o vi" >> $home/.bashrc
+    echo "export TERM=screen-256color-bce" >> $home/.bashrc
+
+    # Tmux
+    apt-get install -y tmux
+    # Tmux - Config
+    mkdir -p $home/.config/tmux
+    git clone https://github.com/xRetry/tmux.git $home/.config/tmux
+
+    # Neovim
+    curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+    tar xzvf nvim-linux64.tar.gz
+    mkdir -p $home/.local/share/nvim-linux64
+    mv nvim-linux64 $home/.local/share/
+    ln -s $home/.local/share/nvim-linux64/bin/nvim /usr/local/bin/nvim
+    rm nvim-linux64.tar.gz
+    # Neovim - Config
+    mkdir -p $home/.config/nvim
+    git clone https://github.com/xRetry/nvim.git $home/.config/nvim
+    nvim --headless +'Lazy sync' +'sleep 20' +qall
+}
+
+apt-get update && apt-get install -y \
+    curl \
+    git \
+    g++ \
+    unzip
+
+user=$(id -u -n)
+
+while getopts "t:l:" opt 
+do
+    case $opt in
+        l) 
+	    parse_lang $OPTARG $user
+            ;;
+        t) 
+	    user=$OPTARG
+            setup_tools $user
+            ;;
+    esac
 done
 
 exit 0
