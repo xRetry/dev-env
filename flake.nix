8098459428f92abbc81163bd3eab59068bd68158
xRetry <marcoschwarz.at@gmail.com>
2023-11-20 21:10:11 +0100

    Fix nix dev-env

--- flake.nix
+++ flake.nix
@@ -4,32 +4,32 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         flake-utils.url = "github:numtide/flake-utils";
-        nvim-config.url = "github:XRetry/nvim";
-        tmux-config.url = "github:XRetry/tmux";
+        nvim-custom.url = "github:XRetry/nvim";
+        tmux-custom.url = "github:XRetry/tmux";
     };
 
-    outputs = { self, nixpkgs, flake-utils, nvim-config, tmux-config, ... }:
+    outputs = { self, nixpkgs, flake-utils, nvim-custom, tmux-custom, ... }:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system:
     let
         pkgs = import nixpkgs {
             inherit system;
             overlays = [ 
-                nvim-config.overlays.default 
-                tmux-config.overlays.default 
+                nvim-custom.overlays.default 
+                tmux-custom.overlays.default
             ];
         }; 
     in rec {
         devShells = {
             default = pkgs.mkShell {
                 buildInputs = with pkgs; [
-                    tmux
-                    neovim
+                    tmux-configured
+                    neovim-configured
                 ];
             };
             cpp = pkgs.mkShell {
                 buildInputs = with pkgs; [
-                    tmux
-                    neovim
+                    tmux-configured
+                    neovim-configured
                     gcc
                     clang-tools
                 ];
